16596fb30b0864c23aaffae335a90e48022b5210
eswaribala <parameswaribala@gmail.com>
2022-04-18 15:37:57 +0530

    updated

--- scripts/personal.js
+++ scripts/personal.js
@@ -1,13 +1,16 @@
 var titles=new Array("Select","Mr","Mrs", "Miss","Ms");
-
+var branches=new Array("Select","Adyar","Thiruvanmiyur","Guindy","Avadi")
 window.addEventListener('load',function(){
     //window.alert("Document loaded....");
     var selectElement=document.querySelector("#titleData");
+    var branchElement=document.querySelector("#branch");
 
     titles.forEach(title=>{
         selectElement.add(new Option(title));
     })
-
+    branches.forEach(branch=>{
+        branchElement.add(new Option(branch));
+    })
 
 })
 
