5c9aaf81b1bb89aa3b4b6417b30c755ebb85ee4f
eswaribala <parameswaribala@gmail.com>
2022-04-18 15:21:48 +0530

    updated

--- scripts/personal.js
+++ scripts/personal.js
@@ -3,15 +3,7 @@ var titles=new Array("Mr","Mrs", "Miss","Ms");
 window.addEventListener('load',function(){
     //window.alert("Document loaded....");
     var selectElement=document.querySelector("#titleData");
-/*
-    for(var i=0;i<titles.length;i++){
-       //option=document.createElement('option');
-        //optionText=document.createTextNode(titles[i]);
-        console.log(titles[i]);
-       // option.appendChild(optionText);
-        selectElement.add(new Option(titles[i]));
-    }
-    */
+
     titles.forEach(title=>{
         selectElement.add(new Option(title));
     })
