f6e1912590fd2a8142c03c5282999b3b49ff4983
eswaribala <parameswaribala@gmail.com>
2022-04-19 11:18:59 +0530

    updated

--- scripts/personal.js
+++ scripts/personal.js
@@ -25,6 +25,7 @@ window.addEventListener('load',function(){
 function validate(){
     var selectElement=document.querySelector("#titleData");
     var branchElement=document.querySelector("#branch");
+    var timeElement=document.querySelector("#contactTime");
     console.log(selectElement.value);
     console.log(branchElement.value);
     if((selectElement.value==0)&&(branchElement.value==0)) {
@@ -32,8 +33,33 @@ function validate(){
         branchElement.setAttribute("style","background-color:red;");
         return false;
     }
-   else
-       return true;
+   else {
+       var title=selectElement.value;
+       var branch=branchElement.value
+       var firstName=document.querySelector("#firstName").value;
+        var lastName=document.querySelector("#lastName").value;
+        var countryName=document.querySelector("#countryName").value;
+        var phoneNo=document.querySelector("#phoneNo").value;
+        var email=document.querySelector("#email").value;
+        var contactTime=timeElement.value;
+        var customerGroup=document.querySelector('[name="customer_group"]:checked').value;
+
+        var contactObj={
+            "title":title,
+            "branch":branch,
+            "firstName":firstName,
+            "lastName":lastName,
+            "country":countryName,
+            "phone":phoneNo,
+            "email":email,
+            "time":contactTime,
+            "type":customerGroup
+        }
+        //storage
+        window.sessionStorage.setItem("contact",contactObj);
+        window.alert("form submitted.....");
+        return true;
+    }
 }
 
 
