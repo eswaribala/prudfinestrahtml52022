c9c51998fecc32dbe8e51b47e137e568bc1fb3a5
eswaribala <parameswaribala@gmail.com>
2022-04-19 12:48:17 +0530

    updated

--- scripts/galleryupload.js
+++ scripts/galleryupload.js
@@ -4,10 +4,10 @@ window.addEventListener('load',function(){
 
     var btnRef=document.querySelector('#upload');
     btnRef.addEventListener('click',function (){
-       alert('button clicked....');
+      // alert('button clicked....');
         filesRef=document.querySelector('#gallery');
         pattern=/image.*/
-
+        window.localStorage.setItem('count',filesRef.files.length);
         for(let i=0;i<filesRef.files.length;i++){
             if(filesRef.files[i].type.match(pattern)){
                 console.log("It is an image file");
@@ -17,6 +17,16 @@ window.addEventListener('load',function(){
 
     })
 
+    var downloadBtnRef=document.querySelector('#download');
+    downloadBtnRef.addEventListener('click',function (){
+
+        var count=window.localStorage.getItem('count');
+        for(let i=0;i<count;i++) {
+            if(window.localStorage.getItem('photo'+i)!=undefined){
+                displayphotos(window.localStorage.getItem('photo'+i));
+            }
+        }
+    })
 
 })
 
@@ -29,4 +39,16 @@ function storeFile(index,file){
     fileReader.onload=function(){
         window.localStorage.setItem("photo"+index,fileReader.result);
     }
+}
+
+function displayphotos(fileName){
+
+    var image=new Image();
+    var photoArea=document.querySelector('#photos');
+    image.onload=function(){
+     photoArea.appendChild(image);
+    }
+    image.src=fileName;
+    image.width=100;
+    image.height=100;
 }
