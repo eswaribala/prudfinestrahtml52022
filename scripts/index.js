d8bc7242cb8b2e290b8ec0f7286f9f6f2fd8c2e6
eswaribala <parameswaribala@gmail.com>
2022-04-20 12:59:31 +0530

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,45 @@ window.addEventListener('load',function(){
     var colorArray=new Array('black','yellow','green')
     var centerX=canvas.width/2;
     var centerY=canvas.height/2;
-    var x=-75;
+    var x=-50;
     var y=0;
     for(let i=0;i<colorArray.length;i++){
         canvasContext.beginPath();
-        canvasContext.arc(centerX+x,centerY+y,50,0,
+        canvasContext.arc(centerX+x,centerY+y,35,0,
             2*Math.PI,false)
-        canvasContext.lineWidth=5;
+        canvasContext.lineWidth=2;
         canvasContext.strokeStyle=colorArray[i];
         canvasContext.stroke();
         //canvasContext.closePath();
-        x=x+75;
+        x=x+25;
     }
+    var colorArray=new Array('brown','blue');
+    var x=-35;
+    var y=35;
+    for(let i=0;i<colorArray.length;i++){
+        canvasContext.beginPath();
+        canvasContext.arc(centerX+x,centerY+y,35,0,
+            2*Math.PI,false)
+        canvasContext.lineWidth=2;
+        canvasContext.strokeStyle=colorArray[i];
+        canvasContext.stroke();
+        //canvasContext.closePath();
+        x=x+35;
+    }
+    canvasContext.font = 'italic 24pt Calibri';
+    canvasContext.fillStyle = "blue";
+    canvasContext.fillText("Olympic Ring",centerX-95,centerY-45);
 
+    var canvas=document.querySelector('#flag');
+    var canvasContext=canvas.getContext('2d');
 
+    var grd=canvasContext.createLinearGradient(0,0,canvas.width,canvas.height);
+    grd.addColorStop(0.0,'orange');
+    grd.addColorStop(0.33,'white');
+    grd.addColorStop(0.66,'green');
+
+    canvasContext.rect(0,0,canvas.width,canvas.height);
+    canvasContext.fillStyle=grd;
+    canvasContext.fill();
 
 })
